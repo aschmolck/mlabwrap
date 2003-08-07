54b0eb8bce54d1a3836f2cc9efd4015975ce6d30
Alexander Schmolck <a.schmolck@gmx.net>
2003-08-07 16:18:47 +0000

    Pre-Official version ------------------------------------ o removed dodgy missing-modules crap o added windoze support (untested) o fixed some other stuff

--- setup.py
+++ setup.py
@@ -1,90 +1,99 @@
 #!/usr/bin/env python
-# Variables you might have to change for your installation
-MATLAB_DIR=None
-EXTRA_COMPILE_ARGS=None
-PYTHON_INCLUDE_DIR = None #XXX where to find Numeric/*.h 
+##############################################################################
+########################### setup.py for mlabwrap ############################
+##############################################################################
+## o author: Alexander Schmolck (a.schmolck@gmx.net)
+## o created: 2003-08-07 17:15:22+00:40
+## o last modified: $Date$
+
+####################################################################
+##### VARIABLES YOU MIGHT HAVE TO CHANGE FOR YOUR INSTALLATION #####
+##### (if setup.py fails to guess the right values for them)   #####
+####################################################################
+MATLAB_DIR=None             # e.g: '/usr/local/matlab'; 'c:/matlab6'
+EXTRA_COMPILE_ARGS=None     # e.g: ['-G']
+PLATFORM_DIR=None           # e.g: 'glnx86'; r'win32/microsoft/msvc60'
+
+# hopefully these 3 won't need modification
+MATLAB_LIBRARIES=None       # e.g: ['eng', 'mx', 'mat', 'mi', 'ut']
+PYTHON_INCLUDE_DIR=None     # where to find Numeric/*.h
+
+SUPPORT_MODULES= ["awmstools", "awmsmeta"] # set to [] if already 
+                                           # installed
+
+# DON'T FORGET TO DO SOMETHING LIKE:
+#   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/MATLAB_DIR/extern/lib/glnx86/
+####################################################################
+### NO MODIFICATIONS SHOULD BE NECESSARY BEYOND THIS POINT       ###
+####################################################################
+# *******************************************************************
 from distutils.core import setup, Extension
 from distutils.file_util import copy_file
-import sys, os, os.path, shutil
-import re
-shove = lambda src, dst: copy_file(src, dst, preserve_mode=1, preserve_times=1)
+import sys, os, os.path, glob, re
 if sys.version_info < (2,2):
     print >> sys.stderr, "You need at least python 2.2"
     sys.exit(1)
-def whichAreMissing(modules):
-    from distutils.version import StrictVersion as Version
-    missing = {}
-    for name, version in modules:
-        try:
-            module = __import__(name)
-            if Version(module.__version__) < Version(version):
-                raise ImportError
-        except ImportError, AttributeError:
-            missing[name] = 1
-    return missing.keys()
 
-# no modifications should be necessary beyond this point
+# windows
 if sys.platform.startswith('win'):
-    print >> sys.stderr, """\
-WINDOZE INSTALL VIA SETUP.PY CURRENTLY UNSUPPORTED
-PLEASE CONTRIBUTE.
-"""
-    sys.exit(1)
-
-# try to guess where matlab is
-if not MATLAB_DIR:
-    for path in ['/usr/local/matlab',
-                 re.sub(r'/bin/.*', '', os.popen('which matlab').read())]:
-        if os.path.exists(path):
-            MATLAB_DIR = path
-            break
-    else:
-        print >> sys.stderr, """\
+    MATLAB_LIBRARIES = MATLAB_LIBRARIES or 'libeng libmx'.split()
+    print >> sys.stderr, "WINDOZE INSTALL UNTESTED: best of luck!"
+    if not MATLAB_DIR:
+        try:
+            MATLAB_DIR = glob.glob('c:/matlab*')[0]
+        except IndexError:
+            print >> sys.stderr, """\
 ERROR: CAN'T FIND MATLAB DIR
 please edit setup.py by hand and set MATLAB_DIR
 """
+            sys.exit(1)
+    PLATFORM_DIR = PLATFORM_DIR or 'win32/microsoft/msvc60'
+# unices
+else:
+    MATLAB_LIBRARIES = MATLAB_LIBRARIES or \
+                       'eng mx mat mi ut'.split()  #FIXME stdc++?
+    # try to guess where matlab is
+    if not MATLAB_DIR:
+        try:
+            guess = os.path.dirname(os.popen('which matlab').read()).\
+                    replace('/bin','') or \
+                    filter(None,
+                           map(glob.glob, ['/usr/local/matlab*',
+                                           '/opt/matlab*']))[0][0]
+        except:
+            print >> sys.stderr("FAILED GUESSING MATLAB_DIR, assume default")
+            guess = '/usr/local/matlab'
+        if os.path.exists(guess):
+            MATLAB_DIR = guess
+        else:
+            print >> sys.stderr, """\
+ERROR: CAN'T FIND MATLAB DIR
+please edit setup.py by hand and set MATLAB_DIR
+"""
+            sys.exit(1)
+    if sys.platform.startswith('sunos'):
+        EXTRA_COMPILE_ARGS = EXTRA_COMPILE_ARGS or ['-G']
+        PLATFORM_DIR = PLATFORM_DIR or "sol2"
+    elif sys.platform.startswith('linux'):
+        PLATFORM_DIR = PLATFORM_DIR or "glnx86"
 
-if sys.platform.startswith('sunos'):
-    MATLAB_LIBRARY_DIRS = [MATLAB_DIR + "/extern/lib/sol2"]
-    EXTRA_COMPILE_ARGS = ['-G'] #FIXME
-elif sys.platform.startswith('linux'):
-    MATLAB_LIBRARY_DIRS = [MATLAB_DIR + '/extern/lib/glnx86']
-    
+MATLAB_LIBRARY_DIRS = [MATLAB_DIR + "/extern/lib/" + PLATFORM_DIR]
+MATLAB_INCLUDE_DIRS = [MATLAB_DIR + "/extern/include"] #, "/usr/include"
 
-
-MATLAB_LIBRARIES = 'eng mx mat mi ut'.split()
-
-#FIXME /usr/include
-MATLAB_INCLUDE_DIRS = [MATLAB_DIR + "/extern/include", "/usr/include"]
-
-requiredModules = [('awmsmeta', '0.1a1'),
-                   ('awmstools', '0.7')]
-missingModules = whichAreMissing(requiredModules)
-# Hack to make the automatic building of sourceDists etc. easier
-if os.getenv("BUILD_DIST"):
-    print "BUILDING DIST"
-    missingModules = [t[0] for t in requiredModules]
-    print "added: %r"
-if missingModules:
-    for missingModule in missingModules:
-        shove(os.path.join("others",missingModule + ".py"), "missing")
 setup (# Distribution meta-data
        name = "mlabwrap",
-       version = "0.9b1",
+       version = "0.9b2",
        description = "A high-level bridge to matlab",
        author = "Alexander Schmolck",
        author_email = "A.Schmolck@gmx.net",
        url = "http://alexander.schmolck.org/python/code",
-       package_dir = {'': 'missing'},
-       packages=['.'],
-       ext_modules = 
-           [Extension('mlabrawmodule', ['mlabraw.cpp'],
-                      library_dirs=MATLAB_LIBRARY_DIRS ,
-                      libraries=MATLAB_LIBRARIES,
+       py_modules = ["mlabwrap"] + SUPPORT_MODULES,
+       ext_modules = [
+           Extension('mlabrawmodule', ['mlabraw.cpp'],
+              library_dirs=MATLAB_LIBRARY_DIRS ,
+              libraries=MATLAB_LIBRARIES,
                       include_dirs=MATLAB_INCLUDE_DIRS + (PYTHON_INCLUDE_DIR and [PYTHON_INCLUDE_DIR] or []),
                       extra_compile_args=EXTRA_COMPILE_ARGS,
                       ),
            ]
       )
-shutil.rmtree("missing")
-os.mkdir("missing")
