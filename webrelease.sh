f08d52d5f3736e7a371069302bc33f7480938d5c
Alexander Schmolck <a.schmolck@gmx.net>
2009-03-19 22:57:04 +0000

    Specify you want it pushed; otherwise just htmlize

--- webrelease.sh
+++ webrelease.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-rst2html README.txt --stylesheet=Steely.css index.html && scp index.html Steely.css surface-plot.png ugly-plot.png aschmolck@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
+rst2html README.txt --stylesheet=Steely.css index.html
+[ -z $1 ] && exit 0;
+scp index.html Steely.css surface-plot.png ugly-plot.png aschmolck@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
+
