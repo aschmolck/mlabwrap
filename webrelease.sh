7184f25ed7f34cbd947eb8f7f784ea32168526b2
Alexander Schmolck <a.schmolck@gmx.net>
2009-03-24 02:21:22 +0000

    added new sf stuff as well as new pics

--- webrelease.sh
+++ webrelease.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 rst2html README.txt --stylesheet=Steely.css index.html
-[ -z $1 ] && exit 0;
-scp index.html Steely.css surface-plot.png ugly-plot.png aschmolck@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
+[[ -z $1 ]] && exit 0;
+ssh aschmolck,mlabwrap@shell.sourceforge.net create && \
+scp index.html *.css *..png aschmolck@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
 
