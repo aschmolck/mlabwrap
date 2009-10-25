34539e3acf313fc0a0003737db596ea0131f1196
Alexander Schmolck <a.schmolck@gmx.net>
2009-10-25 23:48:29 +0000

    Removed obsolete filerelease.sh util and made sfuser overridable in webrelease.sh.

--- filerelease.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-autoload -U zfinit
-zfinit
-rm dist/*
-pysdist
-export EMAIL_ADDR="a.schmolck@gmx.net"
-zfanon upload.sf.net
-zfcd incoming
-zfput dist/*
--- webrelease.sh
+++ webrelease.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rst2html README.txt --stylesheet=Steely.css index.html
+sfuser=${sfuser:-aschmolck}
+rst2html README.txt --stylesheet=style.css index.html
 [[ -z $1 ]] && exit 0;
-ssh aschmolck,mlabwrap@shell.sourceforge.net create && \
-scp index.html *.css *..png aschmolck@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
+ssh "$sfuser",mlabwrap@shell.sourceforge.net create && \
+scp index.html *.css *.png "$sfuser"@shell.sf.net:/home/groups/m/ml/mlabwrap/htdocs/
 
